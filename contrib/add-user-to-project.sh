f404df30f7a037c3888f8e35f201371a02c009e6
Christian Berendt <berendt@osism.tech>
2023-01-27 21:24:31 +0100

    Use older helper scripts
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- contrib/add-user-to-project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-domain="$1"
-project="$2"
-user="$3"
-
-openstack --os-cloud service role add --project-domain "$domain" --project "$project" --user-domain "$domain" --user "$user" _member_
-openstack --os-cloud service role add --project-domain "$domain" --project "$project" --user-domain "$domain" --user "$user" heat_stack_owner
-openstack --os-cloud service role add --project-domain "$domain" --project "$project" --user-domain "$domain" --user "$user" load-balancer_member
