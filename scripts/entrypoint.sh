c0a53f9820055977ad4bea8c9da2dea4dbc72c51
Andreas Schrogl <ansc@uhurutec.com>
2024-05-27 09:21:36 +0200

    initial Containerfile and entrypoint.sh
    Signed-off-by: Andreas Schrogl <ansc@uhurutec.com>

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+CLOUD=${CLOUD:-admin}
+SCHEDULE=${SCHEDULE:-0 * * * *}
+
+env >> /etc/environment
+# build cronjob entry
+croncmd="cd /openstack-project-manager; python3 src/manage.py --cloud $CLOUD "$@"  > /proc/1/fd/1 2>/proc/1/fd/2"
+
+echo "SHELL=/bin/bash" >> /etc/cron.d/openstack-project-manager
+echo "BASH_ENV=/etc/environment" >> /etc/cron.d/openstack-project-manager
+echo "$SCHEDULE root $croncmd" >> /etc/cron.d/openstack-project-manager
+
+echo "$@"
+
+# execute CMD
+#exec "$@"
+
+
+cron -l 2 -f
